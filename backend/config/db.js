1e95ed6cd968f84cac085eac44864bbb3361fccb
Kamran <152558522+kamrranayub@users.noreply.github.com>
2026-02-13 03:42:16 +0500

    Refactor MongoDB connection string
    Updated MongoDB connection string to a more generic format.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(
-            'mongodb://kami:kamran123@ac-kbdpk2t-shard-00-00.zror1yy.mongodb.net:27017,ac-kbdpk2t-shard-00-01.zror1yy.mongodb.net:27017,ac-kbdpk2t-shard-00-02.zror1yy.mongodb.net:27017/pood-fanda?ssl=true&replicaSet=atlas-eyv9xj-shard-0&authSource=admin&retryWrites=true&w=majority'
+            'mongodb://'
         );
         console.log("MongoDB Connected...");
     } catch (err) {
